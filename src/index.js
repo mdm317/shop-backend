a07c19cff97034992eba8be9757f865c0ff59e76
unknown <mdm317@naver.com>
2020-09-01 18:05:24 +0900

    'create_table'

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './db'
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+const handleListening = () =>
+  console.log(`✅ Listening on: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
